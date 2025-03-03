dec8bde96e2e005df444104f67466f0fbf14fa76
jrdelrio <jrdelriodom@gmail.com>
2025-03-03 18:50:52 -0300

    navbar toggler fixed color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ export const Navbar = () => {
         <nav className="navbar navbar-expand-lg">
             <div className="container d-flex align-items-center justify-content-between">
                 <img src={chiliSitesLogo} alt="Chilisites Logo" className="logo" />
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse justify-content-center font-roboto font-bold" id="navbarsExample07">
+                <div className="collapse navbar-collapse justify-content-center font-roboto font-bold" id="mainNavbar">
                     <ul className="navbar-nav mb-2 mb-lg-0 d-flex align-items-center">
                         <li className="nav-item">
                             <button
